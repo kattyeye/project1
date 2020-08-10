e3b68482c5ca9d19e8e905d0335591499298d701
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2020-08-10 15:39:23 -0400

    weekend

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("HELLO");
+console.log("HELLO THERE");
+var magicnumber= 1;
+var sum = magicnumber + magicnumber
+console.log("sum", sum);
+
+// person object
+var person = {
+  name: "Jenny", // name is a property
+  age: 45,
+  favoriteColor: "blue"
+}
+console.log(person.favoriteColor);
